fb1e7b076b7d23bf35c17b07f268750b3f280d4f
VestalAve <fadhilkbr@gmail.com>
2025-08-30 15:26:27 +0530

    30.08-final-15.26

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Moon} from "lucide-react"
 import { Link } from "react-router-dom"
 import { Sun } from "lucide-react"
@@ -19,15 +19,60 @@ import {useTheme} from "./ThemeProvider"
 import Samantha from "../data/Samantha.png"
 import { ThemeProvider } from './ThemeProvider';
 import { SidebarTrigger } from './ui/sidebar'
+import { Dropdown } from 'antd'
 
+import {Bell , X} from "lucide-react"
+
+const message = [
+    {
+        id:1 ,
+        type:"edit",
+        name: "Gene Hart",
+        file: "report.doc"
+    },
+    {
+        id: 2,
+        type: "delivery",
+        name : "Emery Mckenzie",
+        from : "@Shopper.com",
+        status : "delivered"
+    },
+]
 const Navbar = () => {
+
     const {setTheme} = useTheme()
+    const [open, setOpen] = useState(false);
+    const [messages, setMessages] = useState(false);
   return (
     <div className="p-4 flex items-center justify-between">
       <SidebarTrigger/>
     <div className = "flex items-center gap-4">
-        <Link to="/">Dashboard</Link>
         
+        <DropdownMenu open={open} onOpenChange={setOpen}>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline" size="icon">
+                    <Bell className="h-5 w-5 text-gray-600 dark:text-gray-300" />
+                </Button>
+            </DropdownMenuTrigger>
+                <DropdownMenuContent align="center" className="min-w-100">
+                {/* Header row */}
+                    <div className="flex items-center justify-between w-full p-4 border-b">
+                        <h1 className="text-lg font-bold text-stone-600">Notifications</h1>
+                        <Button className="bg-black-200" onClick={() => setOpen(false)}><X className ="text-black "/></Button>
+                    </div>
+
+                    {/* Body content */}
+                    <div className="flex flex-col w-full p-4">
+                        <p>Notification 1</p>
+                        <p>Notification 2</p>
+                        {/* More items here */}
+                    </div>
+                </DropdownMenuContent>
+
+            
+        </DropdownMenu>
+
+
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="outline" size="icon">
